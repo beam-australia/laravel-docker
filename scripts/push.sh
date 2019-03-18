8554326f16ed7474f2f1aed5ddcf961c4977dddb
Andrew McLagan <andrewmclagan@gmail.com>
2019-03-18 11:47:06 +1100

    Travis script errors

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
 
 docker tag beamaustralia/php beamaustralia/php:$TRAVIS_TAG
 
