e2bf91d9b595ce75ab195ad16c897efb33e50a6c
Andrew McLagan <andrewmclagan@gmail.com>
2019-06-05 09:28:12 +1000

    Latest tag deploy

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,4 +6,8 @@ docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
 
 docker tag beamaustralia/php beamaustralia/php:$TRAVIS_TAG
 
+docker tag beamaustralia/php beamaustralia/php:latest
+
 docker push beamaustralia/php:$TRAVIS_TAG
+
+docker push beamaustralia/php:latest
